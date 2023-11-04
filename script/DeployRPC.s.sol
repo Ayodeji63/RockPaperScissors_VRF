f5518f53118e196b11196c2dcc70d1b89c2f1153
Ayodeji <olusanyasamson633@gmail.com>
2023-11-04 18:01:56 +0100

    Interactions

--- script/DeployRPC.s.sol
+++ script/DeployRPC.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {RPC} from "../src/RPC.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "./Interactions.s.sol";
 
 contract DeployRPC is Script {
     function run() external returns (RPC, HelperConfig) {
@@ -19,6 +20,24 @@ contract DeployRPC is Script {
             uint deployerKey
         ) = helperConfig.activeNetworkConfig();
 
+        if (subscriptionId == 0) {
+            // Create a new Subscription
+            CreateSubscription createSubscription = new CreateSubscription();
+            subscriptionId = createSubscription.createSubscription(
+                vrfCoordinator,
+                deployerKey
+            );
+
+            // Fund It!
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                vrfCoordinator,
+                subscriptionId,
+                link,
+                deployerKey
+            );
+        }
+
         vm.startBroadcast();
         RPC rpc = new RPC(
             entranceFee,
@@ -28,6 +47,14 @@ contract DeployRPC is Script {
             callbackGasLimit
         );
         vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(
+            address(rpc),
+            vrfCoordinator,
+            subscriptionId,
+            deployerKey
+        );
 
         return (rpc, helperConfig);
     }
---
+++ script/Interactions.s.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import {LinkToken} from "../test/mocks/LinkToken.sol";
+import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
+
+contract CreateSubscription is Script {
+    function CreateSubscriptionUsingConfig() public returns (uint64) {
+        HelperConfig helperConfig = new HelperConfig();
+        (, address vrfCoordinator, , , , , uint deployerKey) = helperConfig
+            .activeNetworkConfig();
+        return createSubscription(vrfCoordinator, deployerKey);
+    }
+
+    function createSubscription(
+        address vrfCoordinator,
+        uint256 deployerKey
+    ) public returns (uint64) {
+        console.log("Creating subscription on ChainId", block.chainid);
+        vm.startBroadcast(deployerKey);
+        uint64 subId = VRFCoordinatorV2Mock(vrfCoordinator)
+            .createSubscription();
+        vm.stopBroadcast();
+        console.log("Your sub Id is", subId);
+        console.log("Please update subscription in HelperConfig");
+        return subId;
+    }
+
+    function run() external returns (uint64) {
+        return CreateSubscriptionUsingConfig();
+    }
+}
+
+contract FundSubscription is Script {
+    uint96 public constant FUND_AMOUNT = 3 ether;
+
+    function fundSubscription(
+        address vrfCoordinator,
+        uint64 subId,
+        address link,
+        uint256 deployerKey
+    ) public {
+        console.log("Fundung subscription ", subId);
+        console.log("Using vrfCoordinator: ", vrfCoordinator);
+        console.log("On ChainID", block.chainid);
+        if (block.chainid == 31337) {
+            vm.startBroadcast(deployerKey);
+            VRFCoordinatorV2Mock(vrfCoordinator).fundSubscription(
+                subId,
+                FUND_AMOUNT
+            );
+            vm.stopBroadcast();
+        } else {
+            vm.startBroadcast(deployerKey);
+            LinkToken(link).transferAndCall(
+                vrfCoordinator,
+                FUND_AMOUNT,
+                abi.encode(subId)
+            );
+            vm.startBroadcast();
+        }
+    }
+
+    function fundSubscriptionConfig() public {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            ,
+            address vrfCoordinator,
+            ,
+            uint64 subId,
+            ,
+            address link,
+            uint256 deployerKey
+        ) = helperConfig.activeNetworkConfig();
+        fundSubscription(vrfCoordinator, subId, link, deployerKey);
+    }
+
+    function run() external {
+        fundSubscriptionConfig();
+    }
+}
+
+contract AddConsumer is Script {
+    function addConsumer(
+        address rpc,
+        address vrfCoordinator,
+        uint64 subId,
+        uint256 deployerKey
+    ) public {
+        console.log("Adding consumer contract: ", rpc);
+        console.log("Using vrfCoordinator: ", vrfCoordinator);
+        console.log("On ChainID: ", block.chainid);
+        vm.startBroadcast(deployerKey);
+        VRFCoordinatorV2Mock(vrfCoordinator).addConsumer(subId, rpc);
+        vm.stopBroadcast();
+    }
+
+    function addConsumerUsingConfig(address rpc) public {
+        HelperConfig helperConfig = new HelperConfig();
+        (
+            ,
+            address vrfCoordinator,
+            ,
+            uint64 subId,
+            ,
+            ,
+            uint deployerKey
+        ) = helperConfig.activeNetworkConfig();
+        addConsumer(rpc, vrfCoordinator, subId, deployerKey);
+    }
+
+    function run() external {
+        address rpc = DevOpsTools.get_most_recent_deployment(
+            "Rpc",
+            block.chainid
+        );
+        addConsumerUsingConfig(rpc);
+    }
+}
